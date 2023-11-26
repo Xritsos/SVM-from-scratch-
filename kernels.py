419fd4a47e074e987c84131e8f8e74b9dc25e900
Xritsos <kobecris24@yahoo.gr>
2023-11-26 17:35:07 +0200

    kernels cleaned

--- kernels.py
+++ kernels.py
@@ -1,6 +1,4 @@
 import numpy as np
-import numexpr as ne
-
 
 
 def poly(coef=None, degree=None, gamma=None):
@@ -14,10 +12,11 @@ def poly(coef=None, degree=None, gamma=None):
     if gamma == None:
         gamma = 1.0
             
-    if gamma <= 0:
+    if gamma <= 0.0:
         raise ValueError("gamma should be positive !")
         
     def f(x1, x2):
+        
         dot = np.dot(x1, x2.T)
         dot = gamma * dot
         
@@ -27,29 +26,26 @@ def poly(coef=None, degree=None, gamma=None):
     
     return f
 
-# def rbf(x, z, gamma):
-#     n = x.shape[0]
-#     m = z.shape[0]
-#     xx = np.dot(np.sum(np.power(x, 2), 1).reshape(n, 1), np.ones((1, m)))
-#     zz = np.dot(np.sum(np.power(z, 2), 1).reshape(m, 1), np.ones((1, n)))   
-      
-#     return np.exp(-(xx + zz.T - 2 * np.dot(x, z.T)) * gamma)
 
 def rbf(gamma, **kwargs):
     
     if gamma == None:
         gamma = 0.5
         
-    if gamma <= 0:
+    if gamma <= 0.0:
         raise ValueError("gamma should be positive !")
     
     def f(x1, x2):
+        
         n = x1.shape[0]
         m = x2.shape[0]
+        
         xx1 = np.dot(np.sum(np.power(x1, 2), 1).reshape(n, 1), np.ones((1, m)))
         xx2 = np.dot(np.sum(np.power(x2, 2), 1).reshape(m, 1), np.ones((1, n))) 
         
-        return np.exp(-(xx1 + xx2.T - 2 * np.dot(x1, x2.T)) * gamma) 
+        result = np.exp(-(xx1 + xx2.T - 2 * np.dot(x1, x2.T)) * gamma) 
+        
+        return result
     
     return f
 
@@ -65,7 +61,7 @@ def linear(**kwargs):
     return f
 
 
-def sigmoid(coef=None, gamma=None):
+def sigmoid(coef=None, gamma=None, **kwargs):
     
     if coef == None:
         coef = 1.0
@@ -73,13 +69,17 @@ def sigmoid(coef=None, gamma=None):
     if gamma == None:
         gamma = 1.0
         
+    if gamma <= 0.0:
+        raise ValueError("gamma should be positive !")
+        
     def f(x1, x2):
-    
-        dot = np.dot(x1.T, x2)
-        dot = gamma * dot + coef
-    
-        result = np.tanh(dot)
-    
+
+        result = np.dot(x1, x2.T)
+        result *= gamma 
+        result += coef
+        
+        np.tanh(result, result) 
+        
         return result
     
     return f
