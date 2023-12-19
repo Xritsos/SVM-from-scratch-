9bf5256b7f75904667588b5044a9d3b55761e39a
Xritsos <kobecris24@yahoo.gr>
2023-12-19 17:33:56 +0200

    cleaned

--- PCA.py
+++ PCA.py
@@ -1,7 +1,5 @@
-import pickle
 import numpy as np
 from numpy import linalg
-from sklearn.decomposition import PCA
 
 
 class PCA_SVD():
@@ -20,11 +18,11 @@ class PCA_SVD():
         min_dim = min(n, f)
         
         if self.n_comp > min_dim:
-            raise ValueError("Number of components should be less or equal to" \
+            raise ValueError("Number of components should be less or equal to " \
                 f"the min dimension! That is: {min_dim} !")
         
+        # first center the data
         self.mean = np.mean(x, axis=0)
-        
         x = x - self.mean
         
         u, sigma, v_trans = linalg.svd(x, full_matrices=False)
@@ -32,11 +30,17 @@ class PCA_SVD():
         # the values of the sigma matrix are already in descending order
         # so our eigenvalues do not need sorting 
         
-        self.eigen_values = sigma ** 2 / (n - 1)
+        self.eigen_values = sigma ** 2
+        
+        # the values of sigma are the eigenvalues for the A.T A matrix
+        # we need the eigenvalues for the A matrix
+        # that is the square of the sigmas
+        
         self.eigen_vectors = v_trans.T
 
 
     def transform(self, x):
+        # center the data 
         x = x - self.mean
         
         x_transformed = np.dot(x, self.eigen_vectors[:, :self.n_comp])
@@ -51,38 +55,3 @@ class PCA_SVD():
         n_variance = [i/total_variance for i in self.eigen_values[:self.n_comp]]
         
         return n_variance
-
-
-
-
-def unpickle(file):
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
-
-
-if __name__ == '__main__':
-    path = './datasets/cifar/data_batch_1'
-    
-    batch = unpickle(path)
-    
-    data = batch[b'data'][:100, :]
-    temp = np.copy(data)
-    
-    my_pca = PCA_SVD(n_components=3)
-    my_pca.fit(x=data)
-    x_new = my_pca.transform(x=data)
-    
-    pca_sk = PCA(3)
-    d_new = pca_sk.fit_transform(temp)
-    
-    print(x_new[:5, 2])
-    print()
-    print(d_new[:5, 2])
-    
-    var = my_pca.explained_var()
-    
-    print()
-    print(f"My explained variance: {np.sum(var)}")
-    print(f"SKlearn's explained variance: {np.sum(pca_sk.explained_variance_ratio_)}")
-    
